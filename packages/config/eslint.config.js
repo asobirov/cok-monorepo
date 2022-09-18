228ff8b0fe0f5a7b826ce49b28c09dcd3bde80ff
Akbarshokh Sobirov <asobirov357@gmail.com>
2022-09-18 20:30:45 +0100

    Minor fixes

--- packages/config/eslint.config.js
+++ packages/config/eslint.config.js
@@ -11,6 +11,7 @@ module.exports = {
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "react/no-children-prop": "off",
   },
   ignorePatterns: ["/node_modules", "/build", "/public/build"],
 };
