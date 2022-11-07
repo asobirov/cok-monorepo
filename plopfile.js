1682e749cb7406398c91c53f1b5c3aedccc48e05
Akbarshokh Sobirov <asobirov357@gmail.com>
2022-11-07 05:20:37 +0000

    add package generation

---
+++ plopfile.js
@@ -0,0 +1,48 @@
+
+const workspaces = ["packages", "packages/utilities", "packages/components", "packages/tools"];
+
+/**
+ * @param {import("plop").NodePlopAPI} plop
+ */
+module.exports = function (plop) {
+    plop.setGenerator("package", {
+        description: "Generates a package",
+        prompts: [
+            {
+                type: "input",
+                name: "packageName",
+                message: "Enter package name:",
+            }, {
+                type: "input",
+                name: "description",
+                message: "The description of this package:",
+            }, {
+                type: "list",
+                name: "outDir",
+                message: "where should this package live?",
+                default: "packages",
+                choices: workspaces,
+            }
+        ],
+        actions: (answers) => {
+            const actions = [];
+
+            if (!answers) {
+                return actions
+            }
+
+            const { packageName, description, outDir } = answers
+
+            actions.push({
+                type: "addMany",
+                templateFiles: "plop/package-template/**",
+                destination: `./{{outDir}}/{{dashCase packageName}}`,
+                base: "plop/package-template",
+                data: { description, packageName, outDir },
+                abortOnFail: true,
+            })
+
+            return actions
+        }
+    })
+}
